7870d6337967f889055bf7d4b76b971cdae86e23
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-07-21 21:04:45 +0500

    Don

--- server/index.js
+++ server/index.js
@@ -12,11 +12,7 @@ configDotenv();
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: "https://book-store-mern-front-henna.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    optionsSuccessStatus: 200
+    origin: "https://book-store-mern-front-henna.vercel.app"
 }))
 
 app.use("/", (req, res) => {
